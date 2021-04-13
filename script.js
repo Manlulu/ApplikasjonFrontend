2f2b09210eb73d8bceb88bd98d0481828db998f3
Kim <5970502+kims123@users.noreply.github.com>
2021-04-13 15:07:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const baseUrl = "http://localhost:8080";
+// const baseUrl = "http://localhost:8080";
 
-// const baseUrl = "https://secure-badlands-30357.herokuapp.com";
+const baseUrl = "https://secure-badlands-30357.herokuapp.com";
 
 function createUser() {
     let user = {
