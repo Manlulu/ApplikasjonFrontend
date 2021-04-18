81362af71105cdff1edcbc619f1721a7be2e21fb
Kim <fletn3000@hotmail.com>
2021-04-18 13:11:05 +0200

    Tar ut Game i egen script fil

--- script.js
+++ script.js
@@ -137,34 +137,7 @@ function fetchMinSide() {
     startBackend(); // TODO kan denne fjernes?
 }
 
-function fetchGame() {
-    let userToken = window.localStorage.getItem("userToken");
 
-    let token = {
-        "token": userToken
-    };
-
-    fetch(baseUrl + "/user", {
-        method: 'post',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(token)
-    })
-        .then(function (response) {
-            return response.text();
-        }).then(function (responseString) {
-        if (responseString.length !== 0) {
-            document.getElementById("minSide_sok_opp_bruker").style.visibility = "visible";
-            return JSON.parse(responseString);
-        }
-    }).catch((error) => {
-        console.log("En error: " + error);
-    });
-
-    startBackend(); // TODO kan denne fjernes?
-}
 
 function findUser() {
     let user = {
