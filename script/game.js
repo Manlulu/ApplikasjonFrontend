848072057b9f7cdbeddc8d6c6aea925717969336
Kim <fletn3000@hotmail.com>
2021-05-02 12:21:44 +0200

    Knapper for å utfordre en spiller

--- script/game.js
+++ script/game.js
@@ -72,3 +72,36 @@ function setupPlayer(data, id) {
     win.innerHTML = "Win: ".bold() + user.win;
     lost.innerHTML = "Lose: ".bold() + user.lost;
 }
+
+function challengePlayer() {
+    let moves = {
+        "token": window.localStorage.getItem("userToken"),
+        "oponentUsername":  document.getElementById("username").value, // TODO - Må endres
+
+        "gameAttack1": document.querySelector('input[name="game_attack_1"]:checked').value,
+        "gameAttack2": document.querySelector('input[name="game_attack_2"]:checked').value,
+        "gameAttack3": document.querySelector('input[name="game_attack_3"]:checked').value,
+        "gameDefend1": document.querySelector('input[name="game_defend_1"]:checked').value,
+        "gameDefend2": document.querySelector('input[name="game_defend_2"]:checked').value,
+        "gameDefend3": document.querySelector('input[name="game_defend_3"]:checked').value
+    };
+
+    $.ajax({
+        url: baseUrl + "/challenge",
+        dataType: "text",
+        type: "post",
+        contentType: "application/json",
+        data: JSON.stringify(moves),
+        success: function (response) {
+            document.getElementById("game-challenge-message").innerHTML = "Player challenged!";
+        },
+        error: function (error) {
+            console.log(error);
+            if (error.status === 400) {
+                document.getElementById("minSide-message").innerHTML = "Fant ikke bruker";
+            } else {
+                document.getElementById("game-challenge-message").innerHTML = "Ukjent feil";
+            }
+        }
+    });
+}
